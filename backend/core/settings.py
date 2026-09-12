435b08d136546ddb879012701026d36b77709178
coragi-py <fabioyuuki8@gmail.com>
2026-09-12 15:05:05 -0300

    chore: correção de importação de parametros do .env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -19,19 +19,19 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_path = BASE_DIR.parent / ".env"
+env_path = BASE_DIR / ".env"
 load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-938%^jk!n7-2x^))!&b_bzp6ue#i0$g%wblo=wa0+52r6@7$rr"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
     "gamification",
     "glossary",
     "learning",
-    "modules",
     "placement",
     "progress",  
 ]
