41216bc38ae8c4e572fc33cf7173f2b2e4e9754c
Antonio Luiz <linsb414@gmail.com>
2026-09-12 17:15:19 -0300

    feature: Implementado modulo de musica, TB_MUSCI, serializers e rotas v1

--- backend/music/views.py
+++ backend/music/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#Api para determinar como a aplicação vai lidar com as aplicações, por antonio 12/09
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Musica
+from .serializers import MusicaSerializer
 
-# Create your views here.
+class MusicaViewSet(viewsets.ModelViewSet):
+    queryset = Musica.objects.all().order_by('-data_criacao')
+    serializer_class = MusicaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Se o usuário estiver logado, associa automaticamente à gravação
+        if self.request.user.is_authenticated:
+            serializer.save(id_usuario=self.request.user)
+        else:
+            serializer.save()
